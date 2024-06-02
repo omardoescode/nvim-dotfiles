58a5628e4c1adf9b8f20238c6b209925d76c79c4
omardoescode <omardoescode@gmail.com>
2024-06-02 09:18:05 +0300

    Fix trouble not working

--- lua/omar/plugins/trouble.lua
+++ lua/omar/plugins/trouble.lua
@@ -1,12 +1,14 @@
 return {
 	"folke/trouble.nvim",
+	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	cmd = "Trouble",
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
+		{ "<leader>xx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
 		{ "<leader>xw", "<cmd>TrbleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
+		{ "<leader>xd", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+		{ "<leader>xq", "<cmd>Trouble quickfix<CR>", desc = "Open trouble quickfix list" },
+		{ "<leader>xl", "<cmd>Trouble loclist<CR>", desc = "Open trouble location list" },
 		{ "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
 	},
 }
