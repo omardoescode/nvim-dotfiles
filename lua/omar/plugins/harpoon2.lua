7a47f8d97f6ecc144e36e52fd0d29af378df184b
omardoescode <omardoescode@gmail.com>
2024-07-29 19:38:04 +0300

    Improve harpoon

--- lua/omar/plugins/harpoon2.lua
+++ lua/omar/plugins/harpoon2.lua
@@ -2,24 +2,57 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = function()
+		local keys = {
+			{
+				"<leader>aa",
+				function()
+					require("harpoon"):list():add()
+				end,
+				desc = "Harpoon File",
+			},
+			{
+				"<leader>as",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Harpoon Quick Menu",
+			},
+			{
+				"<C-P>",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():prev()
+				end,
+				desc = "Harpoon previous",
+			},
+			{
+				"<C-N>",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():next()
+				end,
+				desc = "Harpoon next",
+			},
+		}
+
+		for i = 1, 5 do
+			table.insert(keys, {
+				"<leader>" .. i,
+				function()
+					require("harpoon"):list():select(i)
+				end,
+				desc = "Harpoon to File " .. i,
+			})
+		end
+
+		return keys
+	end,
 	config = function()
 		local harpoon = require("harpoon")
 		harpoon.setup({})
 
-		-- keymaps
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>aa", function()
-			harpoon:list():add()
-		end, { desc = "Add file to harpoon list" })
-		keymap.set("n", "<leader>as", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { desc = "Toggle Harpoon list" })
-		keymap.set("n", "<C-P>", function()
-			harpoon:list():prev()
-		end)
-		keymap.set("n", "<C-N>", function()
-			harpoon:list():next()
-		end)
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
 		local function toggle_telescope(harpoon_files)
@@ -40,7 +73,7 @@ return {
 				:find()
 		end
 
-		keymap.set("n", "<leader>fh", function()
+		vim.keymap.set("n", "<leader>fh", function()
 			toggle_telescope(harpoon:list())
 		end, { desc = "Open harpoon window" })
 	end,
