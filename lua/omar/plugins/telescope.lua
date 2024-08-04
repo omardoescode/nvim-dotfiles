274cb473ab433f3eedcdd1475d0e51d29f3e2e87
omardoescode <omardoescode@gmail.com>
2024-08-04 11:14:33 +0300

    (Telescope) ignore paths in telescope + add key for hidden files

--- lua/omar/plugins/telescope.lua
+++ lua/omar/plugins/telescope.lua
@@ -32,6 +32,15 @@ return {
 					"--column",
 					"--smart-case",
 				},
+				file_ignore_patterns = {
+					"node_modules",
+					"build",
+					"dist",
+					"yarn.lock",
+					".dist",
+					"package-lock.json",
+					".idea",
+				},
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -50,10 +59,11 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files" })
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files in cwd" })
+		keymap.set("n", "<leader>fh", "<cmd>Telescope find_files no_ignore=true<cr>", { desc = "Find hidden files" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Find git files" })
 		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
 		keymap.set("n", "<leader>/", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find Todos" })
